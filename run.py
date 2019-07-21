b62ec3423cf43071c45fdd1816e437599570f625
Steffan Jensen <6hourapp@gmail.com>
2019-07-22 01:33:55 +0200

    Support Windows: Changed host from 0.0.0.0

--- run.py
+++ run.py
@@ -194,5 +194,5 @@ def multibot():
                        following=following, media_count=media_count);
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
 
