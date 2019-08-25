bef3f7a90719bcf9a1a43e6df8103adf734c2f53
Steffan Jensen <6hourapp@gmail.com>
2019-08-25 04:48:43 +0000

    small fix

--- run.py
+++ run.py
@@ -36,33 +36,32 @@ bot.login(username=args.u, password=args.p, proxy=args.proxy, use_cookie=True)
 def face_detection(username):
     x = 0
     ''' Get user media and scan it for a face'''
-#    medias = bot.get_total_user_medias(username)
     user_id = bot.get_user_id_from_username(username)
     medias = bot.get_user_medias(user_id, filtration=False)
     for media in medias:
-        while x < 2:
+        while x < 1:
             try:
                 bot.logger.info(media)
                 path = bot.download_photo(media, folder=username)
                 img = cv2.imread(path)
                 detector = MTCNN()
                 detect = detector.detect_faces(img)
-                print(detect)
+                if not detect:
+                    bot.logger.info("no face detected")
+                    x += 1
+
+                elif detect:
+                    bot.logger.info("there was a face detected")
+                    bot.api.like(media)
+                    display_url = bot.get_link_from_media_id(media)
+                    bot.logger.info("liked " + display_url + " by " + username)
+                    x += 1
+                else:
+                    x += 1
 
             except Exception as e:
                 bot.logger.info(e)
-
-            if not detect:
-                bot.logger.info("no face detected")
                 x += 1
-
-            else:
-                bot.logger.info("there was a face detected")
-                bot.api.like(media)
-                display_url = bot.get_link_from_media_id(media)
-                bot.logger.info("liked " + display_url + " by " + username)
-                x += 1
-
 
 def get_followers():
     pass
@@ -601,4 +600,3 @@ def multibot():
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=None)
-
